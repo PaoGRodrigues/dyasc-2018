06b9378742da36ce4b731835f5baf8d712cc8b59
paogrodrigues <paogrodrigues@gmail.com>
2018-08-17 20:22:23 -0300

    Replaced Tabs for Spaces

--- fibo/src/main/java/ar/edu/untref/dyasc/Program.java
+++ fibo/src/main/java/ar/edu/untref/dyasc/Program.java
@@ -4,19 +4,16 @@ public class Program {
 
     public static void main(String[] args) {
 
-    	int numeroIngresado = Integer.parseInt(args[0]);
-    	
-    	int numero1=0;
-    	int numero2=1;
+        int numeroIngresado = Integer.parseInt(args[0]);
+        int numero1 = 0;
+        int numero2 = 1;
 
-    	System.out.print("Fibo<" + args[0] + ">:" + numero1 + " ");
-    	for(int i=2;i<=numeroIngresado;i++){
-    	     System.out.print(numero2 + " ");
-    	     numero2 = numero1 + numero2;
-    	     numero1 = numero2 - numero1;
-    	}
-    	System.out.println();
+        System.out.print("Fibo<" + args[0] + ">:" + numero1 + " ");
+        for (int i = 2; i <= numeroIngresado; i++) {
+            System.out.print(numero2 + " ");
+            numero2 = numero1 + numero2;
+            numero1 = numero2 - numero1;
+        }
+        System.out.println();
     }
 }
-
-
