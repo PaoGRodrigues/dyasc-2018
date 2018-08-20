ba4f8fd8ffca51161517e45c47c8c71f01bcace1
paogrodrigues <paogrodrigues@gmail.com>
2018-08-20 20:17:55 -0300

    Fixed number of printed numbers

--- fibo/src/main/java/ar/edu/untref/dyasc/Program.java
+++ fibo/src/main/java/ar/edu/untref/dyasc/Program.java
@@ -8,7 +8,7 @@ public class Program {
         int numero1 = 0;
         int numero2 = 1;
 
-        System.out.print("Fibo<" + args[0] + ">:" + numero1 + " ");
+        System.out.print("fibo<" + args[0] + ">: " + numero1 + " ");
         for (int i = 2; i <= numeroIngresado; i++) {
             System.out.print(numero2 + " ");
             numero2 = numero1 + numero2;
