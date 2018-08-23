e8b4837a0f675b188003b263987e9ac18e0c9bc8
paorodrigues <paola.rodrigues@mercadolibre.com>
2018-08-23 01:01:14 -0300

    Adding default function and options

--- fibo2/src/main/java/ar/edu/untref/dyasc/Program.java
+++ fibo2/src/main/java/ar/edu/untref/dyasc/Program.java
@@ -4,11 +4,32 @@ public class Program {
 
     public static void main(String[] args) {
 
-        int numeroIngresado = Integer.parseInt(args[0]);
+        if(args.length==1){
+            
+            orientacion_default(Integer.parseInt(args[0]));
+        }else{
+            String orientacion = args[3];
+            String direccion = args[4];
+            
+            if(orientacion=="v" && direccion=="d"){
+                
+            }else if(orientacion=="h" && direccion=="i"){
+                
+            }else if(orientacion=="v" && direccion=="i"){
+                
+            }else{
+                System.out.println("Opciones no validas.");
+            }
+        }
+        
+        
+    }
+    
+    public static void orientacion_default(int numeroIngresado){
         int numero1 = 0;
         int numero2 = 1;
 
-        System.out.print("fibo<" + args[0] + ">: " + numero1 + " ");
+        System.out.print("fibo<" + numeroIngresado + ">: " + numero1 + " ");
         for (int i = 2; i <= numeroIngresado; i++) {
             System.out.print(numero2 + " ");
             numero2 = numero1 + numero2;
