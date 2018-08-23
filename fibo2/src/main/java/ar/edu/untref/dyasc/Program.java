07f877b9a13c2b9f37de464be04687d865f07bba
paorodrigues <paola.rodrigues@mercadolibre.com>
2018-08-23 00:30:14 -0300

    Adding a new proyect fibo2

---
+++ fibo2/src/main/java/ar/edu/untref/dyasc/Program.java
@@ -0,0 +1,19 @@
+package ar.edu.untref.dyasc;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        int numeroIngresado = Integer.parseInt(args[0]);
+        int numero1 = 0;
+        int numero2 = 1;
+
+        System.out.print("fibo<" + args[0] + ">: " + numero1 + " ");
+        for (int i = 2; i <= numeroIngresado; i++) {
+            System.out.print(numero2 + " ");
+            numero2 = numero1 + numero2;
+            numero1 = numero2 - numero1;
+        }
+        System.out.println();
+    }
+}
