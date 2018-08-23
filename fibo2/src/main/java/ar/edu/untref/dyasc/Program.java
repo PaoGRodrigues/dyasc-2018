928a34c12f59e0366d2f716984c31586f0d421a1
paorodrigues <paola.rodrigues@mercadolibre.com>
2018-08-23 01:26:17 -0300

    Adding new functions and refactoring Fibonacci function

--- fibo2/src/main/java/ar/edu/untref/dyasc/Program.java
+++ fibo2/src/main/java/ar/edu/untref/dyasc/Program.java
@@ -5,51 +5,55 @@ public class Program {
     public static void main(String[] args) {
 
         if(args.length==1){
-            
             orientacionDefault(Integer.parseInt(args[0]));
         }else{
             String orientacion = args[3];
             String direccion = args[4];
             
             if(orientacion=="v" && direccion=="d"){
-                
+                verticalDirecta(Integer.parseInt(args[6]));
             }else if(orientacion=="h" && direccion=="i"){
-                
+                horizontalInversa(Integer.parseInt(args[6]));
             }else if(orientacion=="v" && direccion=="i"){
                 
             }else{
                 System.out.println("Opciones no validas.");
             }
         }
+    }
+    
+    public static int[] fibonacci(int numeroIngresado){
+        int[] serie = new int[numeroIngresado];
+        serie[0]=0;
+        serie[1]=1;
         
-        
+        for(int i=2; i <=numeroIngresado; i++){
+            serie[i] = serie[i-1] + serie[i-2];
+        }
+        return serie;
     }
     
     public static void orientacionDefault(int numeroIngresado){
-        int numero1 = 0;
-        int numero2 = 1;
-
-        System.out.print("fibo<" + numeroIngresado + ">: " + numero1 + " ");
-        for (int i = 2; i <= numeroIngresado; i++) {
-            System.out.print(numero2 + " ");
-            numero2 = numero1 + numero2;
-            numero1 = numero2 - numero1;
+        int[] serie = fibonacci(numeroIngresado);
+        System.out.print("fibo<" + numeroIngresado + ">: ");
+        for(int i=0; i<=serie.length-1; i++){
+            System.out.print(serie[i] + " ");
         }
-        System.out.println();
     }
     
     public static void verticalDirecta(int numeroIngresado){
-        int numero1 = 0;
-        int numero2 = 1;
-
+        int[] serie = fibonacci(numeroIngresado);
         System.out.println("fibo<" + numeroIngresado + ">: ");
-        System.out.println(numero1);
-        for (int i = 2; i <= numeroIngresado; i++) {
-            System.out.println(numero2 + " ");
-            numero2 = numero1 + numero2;
-            numero1 = numero2 - numero1;
+        for (int i=0; i <= serie.length-1; i++) {
+            System.out.println(serie[i] + " ");
         }
-        System.out.println();
-        
+    }
+    
+    public static void horizontalInversa(int numeroIngresado){
+        int[] serie = fibonacci(numeroIngresado);
+        System.out.print("fibo<" + numeroIngresado + ">: ");
+        for(int i=serie.length; i>=0; i--){
+            System.out.print(serie[i] + " ");
+        }
     }
 }
