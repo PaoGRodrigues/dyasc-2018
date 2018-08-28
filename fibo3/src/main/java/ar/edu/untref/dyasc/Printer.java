b25e8ef0e17c38ccb3f1d7f0a005e86a58df9142
paorodrigues <paola.rodrigues@mercadolibre.com>
2018-08-28 00:40:55 -0300

    Fixed Printer

--- fibo3/src/main/java/ar/edu/untref/dyasc/Printer.java
+++ fibo3/src/main/java/ar/edu/untref/dyasc/Printer.java
@@ -1,35 +1,55 @@
 package ar.edu.untref.dyasc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Printer {
 
+    private Map<String, Runnable> options;
+    private int[] serie;
+    
     public Printer(FiboCalculator fibo, Interpreter interpreter){
         System.out.print("fibo<" + fibo.getNumber() + ">: ");
+        this.options = new HashMap<String,Runnable>();
+        completeOptions();
+        this.serie = fibo.getSerie();
     }
     
-    public static void orientacionDefault(){
-        for(int i=0; i<=serie.length-1; i++){
-            System.out.print(serie[i] + " ");
+    private void completeOptions(){
+        this.options.put("vd", ()->this.verticalDirecta());
+        this.options.put("hi", ()->this.horizontalInversa());
+        this.options.put("vi", ()->this.verticalInversa());
+    }
+    
+    private void orientacionDefault(){
+        for(int i=0; i<=this.serie.length-1; i++){
+            System.out.print(this.serie[i] + " ");
         }
     }
     
-    public static void verticalDirecta(int numeroIngresado){
-        System.out.println("fibo<" + numeroIngresado + ">: ");
-        for (int i=0; i <= serie.length-1; i++) {
-            System.out.println(serie[i] + " ");
+    private void verticalDirecta(){
+        for (int i=0; i<=this.serie.length-1; i++) {
+            System.out.println(this.serie[i] + " ");
         }
     }
     
-    public static void horizontalInversa(int numeroIngresado){
-        System.out.print("fibo<" + numeroIngresado + ">: ");
-        for(int i=serie.length-1; i>=0; i--){
-            System.out.print(serie[i] + " ");
+    private void horizontalInversa(){
+        for(int i=this.serie.length-1; i>=0; i--){
+            System.out.print(this.serie[i] + " ");
         }
     }
     
-    public static void verticalInversa(int numeroIngresado){
-        System.out.println("fibo<" + numeroIngresado + ">: ");
-        for (int i=serie.length-1; i>=0; i--) {
-            System.out.println(serie[i] + " ");
+    private void verticalInversa(){
+        for (int i=this.serie.length-1; i>=0; i--) {
+            System.out.println(this.serie[i] + " ");
+        }
+    }
+    
+    public void runCommand(String option){
+        if(this.options.containsKey(option)){
+            this.options.get(option).run();
+        }else{
+            this.orientacionDefault();
         }
     }
 }
