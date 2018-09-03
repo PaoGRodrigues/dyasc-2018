bccb997b273b8a12ca43378c60f3ebb212ae473a
paorodrigues <paola.rodrigues@mercadolibre.com>
2018-09-02 23:53:40 -0300

    Fixing mode options to work with neither s or l

--- fibo3/src/main/java/ar/edu/untref/dyasc/CommandRunner.java
+++ fibo3/src/main/java/ar/edu/untref/dyasc/CommandRunner.java
@@ -1,30 +1,46 @@
 package ar.edu.untref.dyasc;
 
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CommandRunner {
 
     private Interpreter interpreter;
     private FiboCalculator fibo;
+    private Map<String, int[]> modeType;
     
     public CommandRunner(FiboCalculator fibo, Interpreter interpreter){
         this.fibo= fibo;
         this.interpreter = interpreter;
+        this.modeType = new HashMap<String, int[]>();
+        completeModeType();
     }
     
     public void run() throws FileNotFoundException{
         
-        int[] serie;
-        if(this.interpreter.getParameter("-m").equals("s")){
-            PlusProcessor plusProc = new PlusProcessor();
-            serie = new int[1];
-            serie[0] = plusProc.plusSerie(this.fibo.getSerie());
-        }else{
-            serie = this.fibo.getSerie();
-        }
-        
+        int[] serie = this.modeType.get(this.interpreter.getParameter("-m"));
+                
         Printer printer = new Printer(this.fibo,serie);
         printer.runCommand(this.interpreter.getParameter("-o"), this.interpreter.getParameter("-f"));
         
+    }
+    
+    private void completeModeType(){
+        this.modeType.put("l", this.inListMode());
+        this.modeType.put(null, this.inListMode());
+        this.modeType.put("s", this.inPlusMode());
+    }
+    
+    private int[] inListMode(){
+        return this.fibo.getSerie();
+    }
+    
+    private int[] inPlusMode(){
+        PlusProcessor plusProc = new PlusProcessor();
+        int[] serie = new int[1];
+        serie[0] = plusProc.plusSerie(this.fibo.getSerie());
+        
+        return serie;
     }
 }
