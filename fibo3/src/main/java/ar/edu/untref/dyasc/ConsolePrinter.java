074cdb30fa0b581d5926644dabc48578091cf0be
PaoGRodrigues <paogrodrigues@gmail.com>
2018-10-10 00:07:39 -0300

    Fixed Printers

--- fibo3/src/main/java/ar/edu/untref/dyasc/ConsolePrinter.java
+++ fibo3/src/main/java/ar/edu/untref/dyasc/ConsolePrinter.java
@@ -8,4 +8,6 @@ public class ConsolePrinter {
         System.out.print("fibo<" + number + ">: ");
         return System.out;
     }
+    
+    
 }
--- fibo3/src/main/java/ar/edu/untref/dyasc/Printer.java
+++ fibo3/src/main/java/ar/edu/untref/dyasc/Printer.java
@@ -9,55 +9,41 @@ public class Printer {
 
     private Map<String, Runnable> options;
     private FibonacciCalculator fibo;
-    private int[] serie;
+    private int[] series;
     private String file;
     private PrintStream output=null;
 
     public Printer(FibonacciCalculator fibo, int[] serie, String file){
         this.fibo = fibo;
         this.options = new HashMap<String,Runnable>();
-        this.serie = serie;
+        this.series = serie;
         this.file = file;
     }
 
     /*Completes the options' value with the output on them.*/
     private void loadOptionsMap(PrintStream output){
-        this.options.put("vd", ()->this.verticalDirecta(output));
-        this.options.put("hi", ()->this.horizontalInversa(output));
-        this.options.put("vi", ()->this.verticalInversa(output));
-        this.options.put("hd", ()->this.orientacionDefault(output));
+        this.options.put("v", ()->printSeries(output, "\n"));
+        this.options.put("h", ()->printSeries(output, " "));
     }
-
-    private void orientacionDefault(PrintStream output){
-        for(int i=0; i<=this.serie.length-1; i++){
-            output.print(this.serie[i] + " ");
+    
+    private void inverseSeries(){
+        int[] temp = new int[this.series.length];
+        for(int i=0; i<=this.series.length-1; i++){
+            temp[i] = this.series[this.series.length-i-1];
         }
-        output.println();
+        this.series = temp;
     }
-
-    private void verticalDirecta(PrintStream output){
-        output.println();
-        for (int i=0; i<=this.serie.length-1; i++) {
-            output.println(this.serie[i] + " ");
+    
+    private void printSeries(PrintStream output, String space){
+        output.print(space);
+        for(int i = 0; i<=series.length-1; i++){
+            output.print(series[i] + space);
         }
+        output.print(space);
     }
-
-    private void horizontalInversa(PrintStream output){
-        for(int i=this.serie.length-1; i>=0; i--){
-            output.print(this.serie[i] + " ");
-        }
-        output.println();
-    }
-
-    private void verticalInversa(PrintStream output){
-        output.println();
-        for (int i=this.serie.length-1; i>=0; i--) {
-            output.println(this.serie[i] + " ");
-        }
-    }
-
+   
     private boolean isValidOption(String option){
-        return this.options.containsKey(option);
+        return (option.contains("h")||option.contains("v")) && (option.contains("i")||option.contains("d"));
     }
 
     private PrintStream createPrinter() throws FileNotFoundException{
@@ -79,12 +65,29 @@ public class Printer {
         return option;
     }
 
+    public void analyzeDirectOrInverse(String param){
+        if(param.contains("i")){
+            this.inverseSeries();
+        }
+    }
+    
+    public String analyzeHorizontalOrVertical(String param){
+        String orientation = "";
+        if(param.contains("h")){
+            orientation = "h";
+        }else{
+            orientation = "v";
+        }
+        return orientation;
+    }
+    
     public void print(String option) throws FileNotFoundException{
-        String newOption = checkNullOption(option);
-        this.output = this.createPrinter();
-        loadOptionsMap(this.output);
+        String newOption = this.checkNullOption(option);
         if(isValidOption(newOption)){
-            this.options.get(newOption).run();
+            this.output = this.createPrinter();
+            loadOptionsMap(this.output);
+            analyzeDirectOrInverse(newOption);
+            this.options.get(analyzeHorizontalOrVertical(newOption)).run();
         }else{
             System.out.println("Opciones no válidas.");
         }
