cbd98249c4923426eb6a0ce903826ab2e682e28d
PaoGRodrigues <paogrodrigues@gmail.com>
2018-10-15 01:34:33 -0300

    Adding file printer

--- ToolkitLog/src/Bitacora.java
+++ ToolkitLog/src/Bitacora.java
@@ -1,15 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Bitacora {
 
+    private PrintStream impresor;
+    
+    public Bitacora(){
+        this.modificarArchivo("bitacora.txt");
+    }
+    
     public synchronized void registrar(String evento){
         
         LocalDateTime fecha = LocalDateTime.now();
         DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd/mm/yyyy hh:mm");
         String fechaFormateada = fecha.format(formateador);
-        
-        System.out.println(fechaFormateada + " " + evento);
-        
+        String registro = fechaFormateada.concat(" " + evento + "\n");
+        System.out.print(registro);
+        this.impresor.print(registro);
+    }
+    
+    public void modificarArchivo(String archivo){
+        File nuevoArchivo = new File(archivo);
+        try{
+            this.impresor = new PrintStream(nuevoArchivo);
+        }catch(FileNotFoundException e){
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
