d24f920067cd6fe05e2d8a30276434e268f41d3a
PaoGRodrigues <paogrodrigues@gmail.com>
2018-10-18 00:17:59 -0300

    Creating a Tablero and putting a bote on it

---
+++ BatallaNaval/src/BatallaNavalTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class BatallaNavalTest {
+
+    @Test
+    public void ubicarUnBote(){
+        Tablero tablero = new Tablero(8);
+        
+        tablero.ubicarBote("1","A");
+    }
+}
---
+++ BatallaNaval/src/Tablero.java
@@ -0,0 +1,11 @@
+
+public class Tablero {
+
+    public Tablero(int tamano){
+        
+    }
+
+    public void ubicarBote(String fila, String columna) {
+        
+    }
+}
